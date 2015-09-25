d1741d46a3a8a9838101bd7f2a41a6aafe7bb2a5
AoJ <aoj@n13.cz>
2015-09-25 20:35:41 +0000

    fix config

--- install/prepare.sh
+++ install/prepare.sh
@@ -3,7 +3,5 @@ set -e
 
 mkdir -p /data/riak
 
-# cat /tmp/files/riak.local.conf | sed s/riak@127.0.0.1/"riak@`hostname`"/g >> /etc/riak/riak.conf
-# cat /etc/riak/riak.conf
-
+cp /tmp/files/riak.conf /etc/riak/riak.conf
 mv /tmp/files/start.sh /opt/start.sh
